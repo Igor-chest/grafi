841003df8bb4b5842b26fa46b2268261d46039ec
Igor-chest <55881908+Igor-chest@users.noreply.github.com>
2019-12-27 20:26:08 +0500

    Update grafi.py

--- grafi.py
+++ grafi.py
@@ -1,27 +1,36 @@
+def count():
+    with open("input.txt", "r") as f:
+        a = []
+        for i in f.read().split('\n'):
+            a += i.split()
+    return(len(set(a)))
+
+x = count()
+
 def create(count):
     tree = [[ 0 for j in range(count)] for i in range(count)]
-    unic=[]
-    dicUnic={}
+    unic = []
+    dicUnic = {}
     f = open('input.txt','r')
     for line in f:
-	main=''
-	num=''
-	for a in line:
-	    if a not in (' ','\n'):
-		num+=a
-	    else:
-		if int(num) not in unic:
-		    unic.append(int(num))
-		    dicUnic[int(num)]=len(unic)-1
-		if not main:
-		    main = num
-		    num=''
-		else:
-		    tree[dicUnic[int(main)]][dicUnic[int(num)]]=1
-		    num=''
+        main = ''
+        num  = ''
+        for a in line:
+            if a not in (' ', '\n'):
+                num += a
+            else:
+                if int(num) not in unic:
+                    unic.append(int(num))
+                    dicUnic[int(num)] = len(unic)-1
+                if not main:
+                    main = num
+                    num = ''
+                else:
+                    tree[dicUnic[int(main)]][dicUnic[int(num)]] = 1
+                    num = ''
     f.close()
     return tree, dicUnic, unic
-tree, dicUnic, unic=create(5)
+tree, dicUnic, unic = create(x)
 print(tree)
 print(unic)
 
