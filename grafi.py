524c96a0196fb4bbb2db3b902644c9117062e9e5
Allekseev <55903749+Allekseev@users.noreply.github.com>
2019-12-27 19:55:23 +0500

    Update grafi.py

--- grafi.py
+++ grafi.py
@@ -1,16 +1,29 @@
-tree = [[ 0 for j in range(10)] for i in range(10)] 
-
-def file1(): 
-    with open('input.txt', 'r') as f: 
-        for line in f: 
-            main = '' 
-            for x in line: 
-                if main == '': 
-                    main = x 
-                elif x != ' ' and x != '\n': 
-                    tree[int(main)][int(x)] = 1 
-
-file1() 
+def create(count):
+    tree = [[ 0 for j in range(count)] for i in range(count)]
+    unic=[]
+    dicUnic={}
+    f = open('input.txt','r')
+    for line in f:
+	main=''
+	num=''
+	for a in line:
+	    if a not in (' ','\n'):
+		num+=a
+	    else:
+		if int(num) not in unic:
+		    unic.append(int(num))
+		    dicUnic[int(num)]=len(unic)-1
+		if not main:
+		    main = num
+		    num=''
+		else:
+		    tree[dicUnic[int(main)]][dicUnic[int(num)]]=1
+		    num=''
+    f.close()
+    return tree, dicUnic, unic
+tree, dicUnic, unic=create(5)
+print(tree)
+print(unic)
 
 for i in range(len(tree)): 
     for j in range(len(tree)): 
